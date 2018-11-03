4166d95a1ae345d82f2bbc19a1e2e7e5f54800fe
mertcinar <cinar.mrt@gmail.com>
2018-11-03 22:38:42 +0300

    survival mode

--- kubitz-client/src/main/java/kubitz/client/logic/SurvivalMode.java
+++ kubitz-client/src/main/java/kubitz/client/logic/SurvivalMode.java
@@ -3,13 +3,28 @@ package kubitz.client.logic;
 import kubitz.client.components.Card;
 import kubitz.client.components.Cube;
 import kubitz.client.components.Grid;
+import kubitz.client.controllers.CountdownTimeController;
 
 public class SurvivalMode extends BaseGame {
+
+    CountdownTimeController ctc;
+    long time;
+
     public SurvivalMode(Grid grid, Cube cube, Card card) {
         super(grid, cube, card);
+        time = 150000; //ToDo reasonable time period
+        ctc = new CountdownTimeController(time, this::onGameFinished);
     }
 
     public boolean isGameFinished() {
         return false;
+    }
+
+    public void createNewChallenge(){
+
+    }
+
+    public Void onGameFinished(Void v){
+        return null;
     }
 }
