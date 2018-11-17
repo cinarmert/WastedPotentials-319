c3fd7d027415629e5c9e8d93f770d8fc8d203eb7
Yaman Yagiz Tasbag <yytasbag@gmail.com>
2018-11-18 00:25:17 +0300

    implement survivalmode, move onGameFinished to the constructor since it should notify the UI and should be handled there.

--- kubitz-client/src/main/java/kubitz/client/logic/SurvivalMode.java
+++ kubitz-client/src/main/java/kubitz/client/logic/SurvivalMode.java
@@ -5,26 +5,35 @@ import kubitz.client.components.Cube;
 import kubitz.client.components.Grid;
 import kubitz.client.controllers.CountdownTimeController;
 
+import java.util.function.Function;
+
 public class SurvivalMode extends BaseGame {
 
     CountdownTimeController ctc;
     long time;
+    int score;
+    Function<Void, Void> onGameFinished;
 
-    public SurvivalMode(Grid grid, Cube cube, Card card) {
+    public SurvivalMode(Grid grid, Cube cube, Card card, Function<Void, Void> onGameFinished) {
         super(grid, cube, card);
         time = 150000; //ToDo reasonable time period
-        ctc = new CountdownTimeController(time, this::onGameFinished);
+        ctc = new CountdownTimeController(time, onGameFinished);
+        this.onGameFinished = onGameFinished;
     }
 
     public boolean isGameFinished() {
-        return false;
+        boolean finished = super.isGameFinished();
+        if(finished)
+            createNewChallenge();
+        return finished;
     }
 
-    public void createNewChallenge(){
-
+    public void createNewChallenge()
+    {
+        ctc.addRemainingTime(15000); //TODO reasonable time
+        grid = new Grid(grid.getSize());
+        card = Card.getRandomCard();
+        score++; //TODO maybe change the score system??
     }
 
-    public Void onGameFinished(Void v){
-        return null;
-    }
 }
