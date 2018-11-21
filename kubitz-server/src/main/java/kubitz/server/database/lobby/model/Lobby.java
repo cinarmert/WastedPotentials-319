d236e68cfcf517cf258e01f8f471d176b9834747
Alptekin <alptekin_ay@hotmail.com>
2018-11-22 02:21:25 +0300

    implement lobby

---
+++ kubitz-server/src/main/java/kubitz/server/database/lobby/model/Lobby.java
@@ -0,0 +1,78 @@
+package kubitz.server.database.lobby.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "lobby")
+public class Lobby {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private int noOfPlayers;
+
+    private String mode;
+
+    private boolean privateLobby;
+
+    private String status;
+
+    public Lobby(){}
+
+    public Lobby(int id, String name, String mode, boolean privateLobby) {
+        this.id = id;
+        this.name = name;
+        this.mode = mode;
+        this.privateLobby = privateLobby;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNoOfPlayers() {
+        return noOfPlayers;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public Boolean getPrivateLobby() {
+        return privateLobby;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNoOfPlayers(int noOfPlayers) {
+        this.noOfPlayers = noOfPlayers;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public void setPrivateLobby(Boolean privateLobby) {
+        this.privateLobby = privateLobby;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ kubitz-server/src/main/java/kubitz/server/database/lobby/repository/LobbyRepository.java
@@ -0,0 +1,7 @@
+package kubitz.server.database.lobby.repository;
+
+import kubitz.server.database.lobby.model.Lobby;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LobbyRepository extends MongoRepository<Lobby, Long> {
+}
