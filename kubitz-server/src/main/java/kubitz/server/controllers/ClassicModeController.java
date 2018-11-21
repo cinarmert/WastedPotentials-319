b8c16d657ca3e7ab85ac9bf78ea2a5f4257145f6
mertcinar <cinar.mrt@gmail.com>
2018-11-21 04:58:20 +0300

    implement classic mode controller

--- kubitz-server/src/main/java/kubitz/server/controllers/ClassicModeController.java
+++ kubitz-server/src/main/java/kubitz/server/controllers/ClassicModeController.java
@@ -1,5 +1,8 @@
 package kubitz.server.controllers;
 
+import kubitz.server.database.challenges.repository.ChallengeRepository;
+import kubitz.server.util.JsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +13,19 @@ import org.slf4j.LoggerFactory;
 public class ClassicModeController {
 
     private static final Logger logger = LoggerFactory.getLogger(ClassicModeController.class);
+    private final ChallengeRepository challengeRepository;
+
+    @Autowired
+    public ClassicModeController(ChallengeRepository challengeRepository) {
+        this.challengeRepository = challengeRepository;
+    }
 
     @RequestMapping(value = "/getChallenge/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public String getChallenge(@PathVariable("id") String id) {
-        return null;
+    public String getChallenge(@PathVariable("id") int id) {
+        String response = JsonUtil.toJson(challengeRepository.findChallengeById(id));
+        logger.info("returning the challenge: " + response);
+        return response;
     }
 
 }
--- kubitz-server/src/main/java/kubitz/server/database/challenges/repository/ChallengeRepository.java
+++ kubitz-server/src/main/java/kubitz/server/database/challenges/repository/ChallengeRepository.java
@@ -4,4 +4,7 @@ import kubitz.server.database.challenges.model.Challenge;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ChallengeRepository extends MongoRepository<Challenge, Long> {
+
+    public Challenge findChallengeById(int id);
+
 }
