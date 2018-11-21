c351bc7dbc073eff2e893311e0b221b83bb9eaeb
mertcinar <cinar.mrt@gmail.com>
2018-11-21 03:57:05 +0300

    return void from login

--- kubitz-server/src/main/java/kubitz/server/controllers/AccountController.java
+++ kubitz-server/src/main/java/kubitz/server/controllers/AccountController.java
@@ -4,21 +4,21 @@ package kubitz.server.controllers;
 import kubitz.server.database.accounts.model.Account;
 import kubitz.server.database.accounts.repository.AccountRepository;
 import kubitz.server.util.JsonUtil;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
-import java.util.Map;
-import java.util.logging.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping(value = "/accounts")
 public class AccountController {
 
     private AccountRepository accountRepository;
+    private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 
     @Autowired
     public AccountController(AccountRepository accountRepository) {
@@ -28,18 +28,16 @@ public class AccountController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public String login(@RequestBody String body) {
+    public void login(@RequestBody String body) {
         Account accountToAdd = null;
         try {
             accountToAdd = JsonUtil.fromJson(body, Account.class);
         } catch (IOException e) {
             //Todo add logger - projectwise
-            e.printStackTrace();
-            return JsonUtil.toJson(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null));
+            return;
 
         }
         accountRepository.save(accountToAdd);
-        return JsonUtil.toJson(accountToAdd);
     }
 
 }
