1c6ec5b037639567b68e3bbc7f87d3e6b1895024
mertcinar <cinar.mrt@gmail.com>
2018-11-23 11:05:51 +0300

    change type of id to string

--- kubitz-server/src/main/java/kubitz/server/database/gamestate/model/GameState.java
+++ kubitz-server/src/main/java/kubitz/server/database/gamestate/model/GameState.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class GameState {
 
     @Id
-    private int id;
+    private String id;
 
     private int size;
 
@@ -25,11 +25,11 @@ public class GameState {
         this.size = size;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
