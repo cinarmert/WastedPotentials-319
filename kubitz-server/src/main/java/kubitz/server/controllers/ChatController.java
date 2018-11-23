70a248f5e67e35961bd78c1201ea29cafc952834
mertcinar <cinar.mrt@gmail.com>
2018-11-23 11:11:12 +0300

    implement chat repository

--- kubitz-server/src/main/java/kubitz/server/controllers/ChatController.java
+++ kubitz-server/src/main/java/kubitz/server/controllers/ChatController.java
@@ -1,25 +1,44 @@
 package kubitz.server.controllers;
 
+import kubitz.server.database.chat.model.Message;
+import kubitz.server.database.chat.repository.ChatRepository;
+import kubitz.server.util.JsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(value = "/chat")
 public class ChatController {
 
     private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+    private final ChatRepository chatRepository;
+
+    @Autowired
+    public ChatController(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
 
     @RequestMapping(value = "/getMessages/{ownId}/{lobbyId}", method = RequestMethod.GET)
     @ResponseBody
     public String getMessages(@PathVariable("ownId") String userId, @PathVariable("lobbyId") String lobbyId) {
-        return null;
+        return JsonUtil.toJson(chatRepository.findAllByLobbyId(lobbyId));
     }
 
     @RequestMapping(value = "/postMessage", method = RequestMethod.POST)
     @ResponseBody
     public void postMessage(@RequestBody String body) {
-
+        Message message = null;
+        try {
+             message = JsonUtil.fromJson(body, Message.class);
+        } catch (IOException e) {
+            logger.error("Could not parse body to message, body: " + body);
+            return;
+        }
+        chatRepository.save(message);
     }
 
 }
--- kubitz-server/src/main/java/kubitz/server/database/chat/repository/ChatRepository.java
+++ kubitz-server/src/main/java/kubitz/server/database/chat/repository/ChatRepository.java
@@ -3,6 +3,9 @@ package kubitz.server.database.chat.repository;
 import kubitz.server.database.chat.model.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ChatRepository extends MongoRepository<Message, Long> {
+    List<Message> findAllByLobbyId(String lobbyId);
 
 }
