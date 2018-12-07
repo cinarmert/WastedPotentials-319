4d2cc83ccc1d7e2b2162acf1b5ee5828ecc0073c
mertcinar <cinar.mrt@gmail.com>
2018-12-07 04:04:17 +0300

    add websocket manager

--- kubitz-client/src/main/java/kubitz/client/main/Runner.java
+++ kubitz-client/src/main/java/kubitz/client/main/Runner.java
@@ -4,6 +4,7 @@ package kubitz.client.main;
 import kubitz.client.gui.MainFrame;
 import kubitz.client.util.JsonUtil;
 import kubitz.client.websocket.KubitzWebSocketClient;
+import kubitz.client.websocket.WebSocketManager;
 import kubitz.client.websocket.storage.ChangeSettingsMessage;
 import kubitz.client.websocket.storage.LobbyMessage;
 import kubitz.client.websocket.storage.LobbyMessageTypes;
@@ -20,29 +21,7 @@ public class Runner {
 
     public static void main(String[] args) throws URISyntaxException, InterruptedException {
         new MainFrame();
-        //RESTRequestManager.login(new Account(45, "mert2cli"));
-
-        KubitzWebSocketClient client = new KubitzWebSocketClient(new URI("ws://localhost:8083/player/33" ), new Draft_6455());
-
-
-        LobbyMessage lobbyMessage = new LobbyMessage();
-        lobbyMessage.setType(LobbyMessageTypes.LOBBY_CHANGE_SETTINGS);
-
-        ChangeSettingsMessage changeSettingsMessage = new ChangeSettingsMessage();
-        changeSettingsMessage.setGameMode("test");
-        changeSettingsMessage.setLobbySize("6");
-        changeSettingsMessage.setLobbyId("testLobby");
-        changeSettingsMessage.setPlayerId("bentest");
-        changeSettingsMessage.setPrivateLobby(false);
-        changeSettingsMessage.setStatus("statutest");
-        changeSettingsMessage.setPlayers(new ArrayList<>());
-
-
-        lobbyMessage.setPayload(JsonUtil.toJson(changeSettingsMessage));
-//        client.connectBlocking();
-
-//        client.send(JsonUtil.toJson(lobbyMessage));
-//        while (true){}
+        new WebSocketManager();
     }
 
 }
--- kubitz-client/src/main/java/kubitz/client/websocket/WebSocketManager.java
+++ kubitz-client/src/main/java/kubitz/client/websocket/WebSocketManager.java
@@ -3,6 +3,8 @@ package kubitz.client.websocket;
 import kubitz.client.gui.Config;
 import kubitz.client.util.JsonUtil;
 import kubitz.client.websocket.storage.JoinMessage;
+import kubitz.client.websocket.storage.LobbyMessage;
+import kubitz.client.websocket.storage.LobbyMessageTypes;
 import org.java_websocket.drafts.Draft_6455;
 
 import java.net.URI;
@@ -10,18 +12,24 @@ import java.net.URISyntaxException;
 
 public class WebSocketManager {
 
-    private static KubitzWebSocketClient client;
+    public static KubitzWebSocketClient client;
 
     public WebSocketManager(){
         try {
-            client = new KubitzWebSocketClient(new URI("ws://localhost:8083/player/" + Config.getId()), new Draft_6455());
+            client = new KubitzWebSocketClient(new URI("ws://40.89.169.29:8083/player/" + "101"), new Draft_6455());
+            client.connectBlocking();
         } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public static void sendJoinLobbyMessage(String lobbyId){
         JoinMessage joinMessage = new JoinMessage(Config.getId(), lobbyId, Config.getAccount());
+        LobbyMessage lm = new LobbyMessage();
+        lm.setPayload(JsonUtil.toJson(joinMessage));
+        lm.setType(LobbyMessageTypes.LOBBY_JOIN_MESSAGE);
         client.send(JsonUtil.toJson(joinMessage));
     }
 
