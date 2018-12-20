553a054399cfde0d48dcb5e800377455b6f88af9
mertcinar <cinar.mrt@gmail.com>
2018-12-21 01:51:53 +0300

    add account prop to chatsettingsmsg

--- kubitz-server/src/main/java/kubitz/server/websocket/storage/ChangeSettingsMessage.java
+++ kubitz-server/src/main/java/kubitz/server/websocket/storage/ChangeSettingsMessage.java
@@ -1,12 +1,23 @@
 package kubitz.server.websocket.storage;
 
+import kubitz.server.database.accounts.model.Account;
 import kubitz.server.database.lobby.model.Lobby;
 
 public class ChangeSettingsMessage {
 
     private Lobby lobby;
 
+    private Account account;
+
     public ChangeSettingsMessage(){}
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 
     public Lobby getLobby(){
         return lobby;
