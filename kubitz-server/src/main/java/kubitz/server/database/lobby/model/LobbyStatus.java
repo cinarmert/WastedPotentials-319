0529e9341db9fcd662bbe5e3d28902d36dde893d
Yaman Yagiz Tasbag <yytasbag@gmail.com>
2018-12-23 04:31:44 +0300

    change statuses

--- kubitz-server/src/main/java/kubitz/server/database/lobby/model/LobbyStatus.java
+++ kubitz-server/src/main/java/kubitz/server/database/lobby/model/LobbyStatus.java
@@ -2,6 +2,6 @@ package kubitz.server.database.lobby.model;
 
 public class LobbyStatus
 {
-    public static final String WAITING = "LOBBY_WAITING";
-    public static final String PLAYING = "LOBBY_PLAYING";
+    public static final String WAITING = "LOBBY_STATUS_WAITING";
+    public static final String PLAYING = "LOBBY_STATUS_PLAYING";
 }
